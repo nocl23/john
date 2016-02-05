2f34113eb11d3427857e9390e1472f996eff56be
Matthieu Gaillard <matthieu.gaillard@etudiant.univ-lille1.fr>
2016-02-05 11:28:01 +0100

    Chapitre 3 : TCP

--- webserver/main.c
+++ webserver/main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+#include "socket.h"
 
-int main (int argc, char **argv)
+int main (void)
 {
-	/*Arnold Robbins in the LJ of February '95, describing RCS*/
-	if (argc > 1 && strcmp(argv[1], "-advice") == 0){
-		printf("Don't Panic!\n");
-		return 42;
+	while(1)
+	{
+		creer_serveur(8080);
 	}
-	printf("Need an advice?\n");
 	return 0;
 }
+
