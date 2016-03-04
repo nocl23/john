ce653d873f0ea346e8fd6c7f8f4229bf7fa279ca
Noémie Clay <noemie.clay@hotmail.fr>
2016-03-04 11:36:27 +0100

    tags precedents + fdopen

--- webserver/main.c
+++ webserver/main.c
@@ -42,13 +42,15 @@ int main (void)
 
 	int socket_serveur = creer_serveur(8080);
 	int socket_client ;
-
+	
+	 char msg_client[20];
 	initialiser_signaux();
 
 	while(1)
 	{
 
 		socket_client = accept (socket_serveur, NULL, NULL);
+		
 		if(socket_client == -1)
 		{
 			perror("accept");
@@ -57,11 +59,23 @@ int main (void)
 		int newP; 
 
 		if((newP=fork()) == 0){
+			FILE *flux_socket_client = fdopen(socket_client,"w+");
 
 		/* On peut maintenant dialoguer avec le client */
 			const char * message_bienvenue = "Bonjour, bienvenue sur mon serveur\nLa température exterieure est de 14 °C, il est actuellement 8h46 du matin\nPour ceux qui prennent l'avion direction le serveur de Quentin Porion, il est parti en avance,\nNous vous invitons à vous rediriger vers le serveur de Corwin Nolimittometal qui va être en retard.\nNous espérons que vous avez fait bon voyage à bord de notre avion avec Air NoCl23,\n et nous espérons vous revoir bientôt.\nÀ une prochaine fois !\n";
-			write(socket_client, message_bienvenue, strlen(message_bienvenue));
+			fwrite(message_bienvenue,1,strlen(message_bienvenue),flux_socket_client);
+
+			while(1){
+				
+				fgets( msg_client , sizeof(msg_client),flux_socket_client);
+				fprintf(flux_socket_client,"<john> %s",msg_client);
+				
+
+			}
+
+			fclose(flux_socket_client);
 			close(socket_client);
+
 			exit(0);
 		}else{
 
--- webserver/socket.c
+++ webserver/socket.c
@@ -13,6 +13,8 @@ int creer_serveur(int port)
 {
 	int socket_serveur;
 	socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
+	
+
 		if(socket_serveur == -1)
 	{
 		perror ("socket_serveur");
